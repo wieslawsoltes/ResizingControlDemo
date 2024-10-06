ee3896160957ec1b31408fc41252b6532b1e6c06
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-10-06 23:22:13 +0200

    Handle delete key binding

--- ResizingControlDemo/MainWindow.axaml.cs
+++ ResizingControlDemo/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using ResizingControlDemo.Controls;
 
 namespace ResizingControlDemo;
 
@@ -7,5 +10,22 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        AddHandler(KeyDownEvent, KeyDownHandler, RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+    }
+
+    private void KeyDownHandler(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Delete || e.Key == Key.Back)
+        {
+            var selectedResizingAdornerControl = ResizingHost.GetValue(ResizingHostControl.SelectedResizingAdornerControlProperty);
+            if (selectedResizingAdornerControl is not null)
+            {
+                if (selectedResizingAdornerControl.AdornedElement is Control control)
+                {
+                    EditorCanvas.Children.Remove(control);
+                }
+            }
+        }
     }
 }
