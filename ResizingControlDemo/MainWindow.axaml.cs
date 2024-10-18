5fdc76b8f0042daeee8a3d2fe14fdf46aabf9c55
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-10-18 13:39:58 +0200

    Update MainWindow.axaml.cs

--- ResizingControlDemo/MainWindow.axaml.cs
+++ ResizingControlDemo/MainWindow.axaml.cs
@@ -15,7 +15,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
+
         AddHandler(KeyDownEvent, KeyDownHandler, RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
     }
 
@@ -23,119 +23,128 @@ public partial class MainWindow : Window
     {
         base.OnLoaded(e);
 
+        InitializeProperties();
+
+    }
+
+    private void InitializeProperties()
+    {
         ForegroundColorPicker.IsVisible = false;
         ForegroundColorPicker.ColorChanged += ForegroundColorPickerOnColorChanged;
 
         BackgroundColorPicker.IsVisible = false;
         BackgroundColorPicker.ColorChanged += BackgroundColorPickerOnColorChanged;
- 
-        ResizingHost.GetObservable(ResizingHostControl.SelectedResizingAdornerControlProperty)
-            .Subscribe(new AnonymousObserver<ResizingAdornerControl?>(resizingAdornerControl =>
+
+        ResizingHost
+            .GetObservable(ResizingHostControl.SelectedResizingAdornerControlProperty)
+            .Subscribe(new AnonymousObserver<ResizingAdornerControl?>(UpdateProperties));
+    }
+
+    private void UpdateProperties(ResizingAdornerControl? resizingAdornerControl)
+    {
+        if (resizingAdornerControl is not null)
+        {
+            _selectedControl = resizingAdornerControl.AdornedElement as Control;
+
+            // Foreground
+            switch (_selectedControl)
             {
-                if (resizingAdornerControl is not null)
+                case TextBlock textBlock:
                 {
-                    _selectedControl = resizingAdornerControl.AdornedElement as Control;
-
-                    // Foreground
-                    switch (_selectedControl)
+                    var color = (textBlock.Foreground as SolidColorBrush)?.Color;
+                    if (color is not null)
                     {
-                        case TextBlock textBlock:
-                        {
-                            var color = (textBlock.Foreground as SolidColorBrush)?.Color;
-                            if (color is not null)
-                            {
-                                ForegroundColorPicker.Color = color.Value;
-                                ForegroundColorPicker.IsVisible = true;
-                            }
+                        ForegroundColorPicker.Color = color.Value;
+                        ForegroundColorPicker.IsVisible = true;
+                    }
 
-                            break;
-                        }
-                        case Label label:
-                        {
-                            var color = (label.Foreground as SolidColorBrush)?.Color;
-                            if (color is not null)
-                            {
-                                ForegroundColorPicker.Color = color.Value;
-                                ForegroundColorPicker.IsVisible = true;
-                            }
-                            break;
-                        }
-                        case TextBox textBox:
-                        {
-                            var color = (textBox.Foreground as SolidColorBrush)?.Color;
-                            if (color is not null)
-                            {
-                                ForegroundColorPicker.Color = color.Value;
-                                ForegroundColorPicker.IsVisible = true;
-                            }
-                            break;
-                        }
-                        case Button button:
-                        {
-                            var color = (button.Foreground as SolidColorBrush)?.Color;
-                            if (color is not null)
-                            {
-                                ForegroundColorPicker.Color = color.Value;
-                                ForegroundColorPicker.IsVisible = true;
-                            }
-                            break;
-                        }
-                    }
-                    
-                    // Background
-                    switch (_selectedControl)
-                    {
-                        case TextBlock textBlock:
-                        {
-                            var color = (textBlock.Background as SolidColorBrush)?.Color;
-                            if (color is not null)
-                            {
-                                BackgroundColorPicker.Color = color.Value;
-                                BackgroundColorPicker.IsVisible = true;
-                            }
-                            break;
-                        }
-                        case Label label:
-                        {
-                            var color = (label.Background as SolidColorBrush)?.Color;
-                            if (color is not null)
-                            {
-                                BackgroundColorPicker.Color = color.Value;
-                                BackgroundColorPicker.IsVisible = true;
-                            }
-                            break;
-                        }
-                        case TextBox textBox:
-                        {
-                            var color = (textBox.Background as SolidColorBrush)?.Color;
-                            if (color is not null)
-                            {
-                                BackgroundColorPicker.Color = color.Value;
-                                BackgroundColorPicker.IsVisible = true;
-                            }
-                            break;
-                        }
-                        case Button button:
-                        {
-                            var color = (button.Background as SolidColorBrush)?.Color;
-                            if (color is not null)
-                            {
-                                BackgroundColorPicker.Color = color.Value;
-                                BackgroundColorPicker.IsVisible = true;
-                            }
-                            break;
-                        }
-                    }
-                    
+                    break;
                 }
-                else
+                case Label label:
                 {
-                    ForegroundColorPicker.IsVisible = false;
-                    BackgroundColorPicker.IsVisible = false;
-
-                    _selectedControl = null; 
+                    var color = (label.Foreground as SolidColorBrush)?.Color;
+                    if (color is not null)
+                    {
+                        ForegroundColorPicker.Color = color.Value;
+                        ForegroundColorPicker.IsVisible = true;
+                    }
+                    break;
                 }
-            }));
+                case TextBox textBox:
+                {
+                    var color = (textBox.Foreground as SolidColorBrush)?.Color;
+                    if (color is not null)
+                    {
+                        ForegroundColorPicker.Color = color.Value;
+                        ForegroundColorPicker.IsVisible = true;
+                    }
+                    break;
+                }
+                case Button button:
+                {
+                    var color = (button.Foreground as SolidColorBrush)?.Color;
+                    if (color is not null)
+                    {
+                        ForegroundColorPicker.Color = color.Value;
+                        ForegroundColorPicker.IsVisible = true;
+                    }
+                    break;
+                }
+            }
+                    
+            // Background
+            switch (_selectedControl)
+            {
+                case TextBlock textBlock:
+                {
+                    var color = (textBlock.Background as SolidColorBrush)?.Color;
+                    if (color is not null)
+                    {
+                        BackgroundColorPicker.Color = color.Value;
+                        BackgroundColorPicker.IsVisible = true;
+                    }
+                    break;
+                }
+                case Label label:
+                {
+                    var color = (label.Background as SolidColorBrush)?.Color;
+                    if (color is not null)
+                    {
+                        BackgroundColorPicker.Color = color.Value;
+                        BackgroundColorPicker.IsVisible = true;
+                    }
+                    break;
+                }
+                case TextBox textBox:
+                {
+                    var color = (textBox.Background as SolidColorBrush)?.Color;
+                    if (color is not null)
+                    {
+                        BackgroundColorPicker.Color = color.Value;
+                        BackgroundColorPicker.IsVisible = true;
+                    }
+                    break;
+                }
+                case Button button:
+                {
+                    var color = (button.Background as SolidColorBrush)?.Color;
+                    if (color is not null)
+                    {
+                        BackgroundColorPicker.Color = color.Value;
+                        BackgroundColorPicker.IsVisible = true;
+                    }
+                    break;
+                }
+            }
+                    
+        }
+        else
+        {
+            ForegroundColorPicker.IsVisible = false;
+            BackgroundColorPicker.IsVisible = false;
+
+            _selectedControl = null; 
+        }
     }
 
     private void ForegroundColorPickerOnColorChanged(object? sender, ColorChangedEventArgs e)
@@ -210,25 +219,32 @@ public partial class MainWindow : Window
     {
         if (e.Key == Key.Delete || e.Key == Key.Back)
         {
-            var selectedResizingAdornerControl = ResizingHost.GetValue(ResizingHostControl.SelectedResizingAdornerControlProperty);
-            if (selectedResizingAdornerControl is not null)
+            Delete();
+        }
+    }
+
+    private void Delete()
+    {
+        var selectedResizingAdornerControl = ResizingHost.GetValue(ResizingHostControl.SelectedResizingAdornerControlProperty);
+        if (selectedResizingAdornerControl is null)
+        {
+            return;
+        }
+
+        if (selectedResizingAdornerControl.AdornedElement is Control control)
+        {
+            if (control.Parent is Panel panel)
             {
-                if (selectedResizingAdornerControl.AdornedElement is Control control)
-                {
-                    if (control.Parent is Panel panel)
-                    {
-                        panel.Children.Remove(control);
-                        
-                        ResizingHost.SetValue(ResizingHostControl.SelectedResizingAdornerControlProperty, null);
-                    }
+                panel.Children.Remove(control);
 
-                    if (control.Parent is ContentControl contentControl)
-                    {
-                        contentControl.Content = null;
+                ResizingHost.SetValue(ResizingHostControl.SelectedResizingAdornerControlProperty, null);
+            }
 
-                        ResizingHost.SetValue(ResizingHostControl.SelectedResizingAdornerControlProperty, null);
-                    }
-                }
+            if (control.Parent is ContentControl contentControl)
+            {
+                contentControl.Content = null;
+
+                ResizingHost.SetValue(ResizingHostControl.SelectedResizingAdornerControlProperty, null);
             }
         }
     }
